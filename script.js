e59cfbdfd3dd0eb57d0e4ff34c98b7ca92a4c113
Trzynu <51694373+Trzynastek@users.noreply.github.com>
2022-11-04 19:12:27 +0100

    .

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ http.onload = function(){
 								<i class="fa-solid fa-layer-group platform-icon"> </i>
 								<p class="platforms">${item.platform}</p>
 								<p class="download">${item.download}</p>
-								<i class="fa-solid fa-square-down download-icon"></i>
+								<i class="fa-solid fa-square-down"></i>
 								</div>
 							</div>
 						</div>
