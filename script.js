3aff362d4374fbae9acb3cfcb08061a63c114cf1
Trzynu <51694373+Trzynastek@users.noreply.github.com>
2022-11-05 20:07:24 +0100

    fix

--- script.js
+++ script.js
@@ -112,22 +112,29 @@ function search() {
 				<div class="box snap">
 					<div class="offset item">
 						<div class="coverdiv">
-							<img class="cover" src="covers/${item.image}">
+							<img class="cover" src="covers/${item.image}" loading="lazy">
 						</div>
 						<div class = content>
 							<p class="gametitle">${item.title}</p>
 							<p class="description">${item.description}</p>
 							<div class="tagline">
-								<p class="platforms">${item.platform}</p>
-								<p class="download">${item.download}</p>
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="download-ico" onclick="location.href='${item.link}';"><path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM216 232V334.1l31-31c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-72 72c-9.4 9.4-24.6 9.4-33.9 0l-72-72c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l31 31V232c0-13.3 10.7-24 24-24s24 10.7 24 24z"/></svg>
+								<div class="iconbox">
+									<svg class="staticicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+									<path d="M264.5 5.2c14.9-6.9 32.1-6.9 47 0l218.6 101c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 149.8C37.4 145.8 32 137.3 32 128s5.4-17.9 13.9-21.8L264.5 5.2zM476.9 209.6l53.2 24.6c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 277.8C37.4 273.8 32 265.3 32 256s5.4-17.9 13.9-21.8l53.2-24.6 152 70.2c23.4 10.8 50.4 10.8 73.8 0l152-70.2zm-152 198.2l152-70.2 53.2 24.6c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 405.8C37.4 401.8 32 393.3 32 384s5.4-17.9 13.9-21.8l53.2-24.6 152 70.2c23.4 10.8 50.4 10.8 73.8 0z"/>
+									</svg>
+									<p class="platforms">${item.platform}</p>
+								</div>
+								<div class="iconbox">
+									<p class="download">${item.download}</p>
+									<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" onclick="location.href='${item.link}';">
+									<path d="M256 0C114.6 0 0 114.6 0 256S114.6 512 256 512s256-114.6 256-256S397.4 0 256 0zM244.7 395.3l-112-112c-4.6-4.6-5.9-11.5-3.5-17.4s8.3-9.9 14.8-9.9l64 0 0-96c0-17.7 14.3-32 32-32l32 0c17.7 0 32 14.3 32 32l0 96 64 0c6.5 0 12.3 3.9 14.8 9.9s1.1 12.9-3.5 17.4l-112 112c-6.2 6.2-16.4 6.2-22.6 0z"/>
+									</svg>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-				`;
-			}
+			`;
 		}
 		document.querySelector(".list").innerHTML = output;
  }
